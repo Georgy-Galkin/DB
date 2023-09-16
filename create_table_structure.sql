5df7ee58c6e19a2d6d8c446bb3e6b742cf63c35a
Georgy Galkin <georgy_galkin@mail.com>
2023-09-16 20:25:56 +0300

    added insert statements

--- create_table_structure.sql
+++ create_table_structure.sql
@@ -20,40 +20,6 @@ CREATE TABLE dim_regions (
     "region_name" text NOT NULL
 );
 
--- Define the "dim_dc" table
-CREATE TABLE dim_dc (
-    "region_code" SERIAL FOREIGN KEY REFERENCES dim_regions(region_code),
-    "sku_internal_code" text FOREIGN KEY REFERENCES dim_sku_base(sku_internal_code),
-    "stock_qty" int
-);
-
--- Define the "dim_geo_base" tableeh                                                                                                                         
-CREATE TABLE dim_geo_base (
-    "pos_internal_code" text PRIMARY KEY NOT NULL,
-    "region_code" SERIAL FOREIGN KEY REFERENCES dim_regions(region_code),
-    "city" text,
-    "address" text
-);
-
--- Define the "dim_geo_mapping" table
-CREATE TABLE dim_geo_mapping (
-    "pos_ext_code" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-    "pos_internal_code" text FOREIGN KEY REFERENCES dim_geo_base(pos_internal_code)
-);
-
--- Define the "dim_matrix_goals" table
-CREATE TABLE dim_matrix_goals (
-    "pos_internal_code" text REFERENCES dim_geo_base(pos_internal_code),
-    "matrix" bigint NOT NULL,
-
-);
-
--- Define the "dim_matrix_assortment" table
-CREATE TABLE dim_matrix_assortment (
-    "sku_internal_code" text FOREIGN KEY REFERENCES dim_sku_base(sku_internal_code),
-    "matrix" bigint
-);
-
 -- Define the "dim_sku_base" table
 CREATE TABLE dim_sku_base (
     "sku_internal_code" text PRIMARY KEY NOT NULL,
@@ -61,11 +27,43 @@ CREATE TABLE dim_sku_base (
     "manufacturer" text
 );
 
+-- Define the "dim_dc" table
+CREATE TABLE dim_dc (
+    "region_code" SERIAL REFERENCES dim_regions(region_code),
+    "sku_internal_code" text REFERENCES dim_sku_base(sku_internal_code),
+    "stock_qty" int
+);
+
+-- Define the "dim_geo_base" table
+CREATE TABLE dim_geo_base (
+    "pos_internal_code" text PRIMARY KEY NOT NULL,
+    "region_code" SERIAL REFERENCES dim_regions(region_code),
+    "city" text,
+    "address" text
+);
+
+-- Define the "dim_geo_mapping" table
+CREATE TABLE dim_geo_mapping (
+    "pos_ext_code" text PRIMARY KEY NOT NULL,
+    "pos_internal_code" text REFERENCES dim_geo_base(pos_internal_code)
+);
+
+-- Define the "dim_matrix_goals" table
+CREATE TABLE dim_matrix_goals (
+    "pos_internal_code" text REFERENCES dim_geo_base(pos_internal_code),
+    "matrix" bigint NOT NULL
+);
+
+-- Define the "dim_matrix_assortment" table
+CREATE TABLE dim_matrix_assortment (
+    "sku_internal_code" text REFERENCES dim_sku_base(sku_internal_code),
+    "matrix" bigint
+);
+
 -- Define the "dim_sku_mapping" table
 CREATE TABLE dim_sku_mapping (
     "sku_ext_code" text PRIMARY KEY,
-    "sku_internal_code" text,
-    FOREIGN KEY ("sku_internal_code") REFERENCES dim_sku_base("sku_internal_code")
+    "sku_internal_code" text REFERENCES dim_sku_base(sku_internal_code)
 );
 
 -- Define the "fact_daily_sales" table
@@ -74,7 +72,7 @@ CREATE TABLE fact_daily_sales (
     "sales_qty_units" double precision,
     "date" date,
     "sku_ext_code" text,
-    "pos_ext_code" uuid
+    "pos_ext_code" text
 );
 
 -- Define the "fact_daily_stock" table
@@ -82,5 +80,5 @@ CREATE TABLE fact_daily_stock (
     "stock_qty" double precision,
     "date" date,
     "sku_ext_code" text,
-    "pos_ext_code" uuid
+    "pos_ext_code" text
 );
